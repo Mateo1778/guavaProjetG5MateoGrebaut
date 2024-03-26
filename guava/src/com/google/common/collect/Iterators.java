564eddc37a079936ef417cc60f9d5dca77e67e8e
Grebaut <mateo.grebaut.etu@univ-lille.fr>
2024-03-26 17:45:57 +0100

    un seul while et if

--- guava/src/com/google/common/collect/Iterators.java
+++ guava/src/com/google/common/collect/Iterators.java
@@ -174,21 +174,13 @@ public final class Iterators {
 
   /** Returns {@code true} if {@code iterator} contains {@code element}. */
   public static boolean contains(Iterator<?> iterator, @CheckForNull Object element) {
-    if (element == null) {
-      while (iterator.hasNext()) {
-        if (iterator.next() == null) {
-          return true;
+        while (iterator.hasNext()) {
+          if ((element == null && iterator.next() == null) || element.equals(iterator.next())) {
+            return true;
+          }
         }
-      }
-    } else {
-      while (iterator.hasNext()) {
-        if (element.equals(iterator.next())) {
-          return true;
-        }
-      }
+      return false;
     }
-    return false;
-  }
 
   /**
    * Traverses an iterator and removes every element that belongs to the provided collection. The
